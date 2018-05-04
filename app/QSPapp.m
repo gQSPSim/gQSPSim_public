988eea021bdf80d9449654a1032cf429368e40b7
Iraj Hosseini <>
2018-05-04 15:00:55 -0700

    changed QSPapp to add path for Matlab Files

--- app/QSPapp.m
+++ app/QSPapp.m
@@ -1,5 +1,7 @@
 function app = QSPapp()
 
+EchoOutput = true;
+
 warning('off','uix:ViewPaneManager:NoView')
 warning('off','MATLAB:table:ModifiedAndSavedVarnames');
 
@@ -30,21 +32,90 @@ if ~isdeployed
     
 end %if ~isdeployed
 
+
+% Get the root directory, based on the path of this file
+RootPath = fileparts(mfilename('fullpath'));
+
+
+%% Set up the paths to add to the MATLAB path
+
+%************ EDIT BELOW %************
+
+% The true/false argument specifies whether the given directory should
+% include children (true) or just itself (false)
+
+rootDirs={...
+    fullfile(RootPath,'../app'),true;... %root folder with children
+    fullfile(RootPath,'../utilities'),true;... %root folder with children
+    fullfile(RootPath,'../FromGenentech'),true;... %root folder with children
+    
+    };
+
+%************ EDIT ABOVE %************
+
+pathCell = regexp(path, pathsep, 'split');
+
+%% Loop through the paths and add the necessary subfolders to the MATLAB path
+for pCount = 1:size(rootDirs,1)
+    
+    rootDir=rootDirs{pCount,1};
+    if rootDirs{pCount,2}
+        % recursively add all paths
+        rawPath=genpath(rootDir);
+        rawPathCell=textscan(rawPath,'%s','delimiter',';');
+        rawPathCell=rawPathCell{1};
+        
+    else
+        % Add only that particular directory
+        rawPath = rootDir;
+        rawPathCell = {rawPath};
+    end
+    
+    % remove undesired paths
+    svnFilteredPath=strfind(rawPathCell,'.svn');
+    slprjFilteredPath=strfind(rawPathCell,'slprj');
+    sfprjFilteredPath=strfind(rawPathCell,'sfprj');
+    rtwFilteredPath=strfind(rawPathCell,'_ert_rtw');
+    
+    % loop through path and remove all the .svn entries
+    for pCount=1:length(svnFilteredPath), %#ok<FXSET>
+        filterCheck=[svnFilteredPath{pCount},...
+            slprjFilteredPath{pCount},...
+            sfprjFilteredPath{pCount},...
+            rtwFilteredPath{pCount}];
+        if isempty(filterCheck)
+            if EchoOutput
+                disp(['Adding ',rawPathCell{pCount}]);
+            end
+            if ispc  % Windows is not case-sensitive
+                if ~any(strcmpi(rawPathCell{pCount}, pathCell))
+                    addpath(rawPathCell{pCount}); %#ok<MCAP>
+                end
+            else
+                if ~any(strcmp(rawPathCell{pCount}, pathCell))
+                    addpath(rawPathCell{pCount}); %#ok<MCAP>
+                end
+            end
+        else
+            % ignore
+        end
+    end
+    
+end
+
+
+
 %% Add java class paths
 
 % Disable warnings
 warning('off','MATLAB:HandleGraphics:ObsoletedProperty:JavaFrame')
 warning('off','MATLAB:Java:DuplicateClass');
 warning('off','MATLAB:javaclasspath:jarAlreadySpecified')
-EchoOutput = true;
 
 if EchoOutput
     disp('Initializing Java paths for UI Widgets');
     disp('---------------------------------------------------');
 end
-
-% Get the root directory, based on the path of this file
-RootPath = fileparts(mfilename('fullpath'));
 
 % Tree controls and XLWRITE
 Paths = {
