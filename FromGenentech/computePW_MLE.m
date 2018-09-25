ba18973f9ce5e438871aee91ba43d2b7530e5615
Justin Feigelman <feigelmj@gene.com>
2018-09-25 07:46:46 -0700

    include second step for prevalence weight computation
    - increases diversity via second QP problem

--- FromGenentech/computePW_MLE.m
+++ FromGenentech/computePW_MLE.m
@@ -4,11 +4,11 @@ function PW = computePW_MLE(X,Y,varargin)
 if nargin>2
    SigY = varargin{1};
 else
-   SigY = diag(0.1 * abs(Y));
+   SigY = diag(0.1 * abs(Y) + 1e-3);
 end
     
 if nargin>3
-    W_max = varargin{2};
+    W_max = 1/varargin{2};
 else
     W_max = 0.1;
 end
@@ -16,5 +16,28 @@ end
 H = X' *inv(SigY) * X;
 f = -(Y' * inv(SigY) * X);
 N = size(X,2);
+LB = zeros(N,1);
+UB = W_max*ones(N,1);
+X0 = 1/N*ones(1,N);
 
-PW = quadprog(H,f',[],[],ones(1,N),1,zeros(N,1),W_max*ones(N,1));
+Aeq=ones(1,N);
+beq=1;
+
+[PW, fval] = quadprog(H,f',[],[],Aeq,beq,LB,UB,X0);
+
+%% reweight to increase diversity of the samples
+
+% second step
+Err=X*PW-Y;
+Del=abs(Y*0.025);
+D = Y;
+
+
+% this is awesome
+A=[X; -X];
+b=[Err+Del+D; -Err+Del-D];
+H=eye(N);
+f=[];
+PW = quadprog(H,f,A,b,Aeq,beq,LB,UB,X0);
+
+
