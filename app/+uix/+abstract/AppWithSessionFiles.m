28490e300dec7488cd33b33edf7c88df50c72426
Justin Feigelman <feigelman@gmail.com>
2019-03-07 15:28:45 -0800

    correct file extension for sessions on mac

--- app/+uix/+abstract/AppWithSessionFiles.m
+++ app/+uix/+abstract/AppWithSessionFiles.m
@@ -275,7 +275,9 @@ classdef (Abstract) AppWithSessionFiles < uix.abstract.AppWindow
                     if iscell(FileName)
                         FileName = FileName{1};
                     end
+%                     ThisFile = fullfile(PathName,[FileName obj.FileSpec{1}]);
                     ThisFile = fullfile(PathName,FileName);
+
                     
                     [FileName,PathName,FilterIndex] = uiputfile(obj.FileSpec, ...
                         'Save as',ThisFile);
@@ -290,7 +292,7 @@ classdef (Abstract) AppWithSessionFiles < uix.abstract.AppWindow
                     end
                     
                     % If it's missing the full FileExt (i.e. on Mac/Linux)
-                    if ~isempty(regexp(FileName,FileExt,'once'))
+                    if isempty(regexp(FileName,FileExt,'once'))
                         FileName = regexp(FileName,'\.','split');
                         if iscell(FileName)
                             FileName = FileName{1};
