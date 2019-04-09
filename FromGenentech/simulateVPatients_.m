7c3cd8dd18857f7ad5b3264f95c3624248336b90
Justin Feigelman <feigelman@gmail.com>
2019-04-09 14:59:44 -0700

    working implementation of parallel simulation with interrupt

--- FromGenentech/simulateVPatients_.m
+++ FromGenentech/simulateVPatients_.m
@@ -113,100 +113,119 @@ function [Results, nFailedSims, StatusOK, Message, Cancelled] = simulateVPatient
                 p = parpool(ParallelCluster);
             end
             q = parallel.pool.DataQueue;
-            afterEach(q, @(jj) updateWaitBar(options, ItemModel, jj) );
-            Cancelled = parallel.pool.Constant(false);
+            listener = afterEach(q, @(jj) updateWaitBar(options.WaitBar, ItemModel, jj) );
+
+            if ~isempty(ParamValues_in)
+                Names = options.paramNames;
+                Values = ParamValues_in;
+            else
+                Names = ItemModel.Names;
+                Values = ItemModel.Values;
+            end
             
-            spmd
-                blockSize = ceil(ItemModel.nPatients / numlabs);
-                block = min(ItemModel.nPatients, ...
-                   ((1+blockSize*(labindex-1)):(blockSize*labindex)));
-                nSim = 0;
-%                 disp(block)
-                for jj = block
-        %             disp([' ', num2str(jj),' '])
-                    % check for user-input parameter values
-                    if ~isempty(ParamValues_in)
-                        Names = options.paramNames;
-                        Values = ParamValues_in;
-                    else
-                        Names = ItemModel.Names;
-                        Values = ItemModel.Values;
-                    end
+            nSim = 0;
+            numlabs = p.NumWorkers;
+            blockSize = max(100,ceil(ItemModel.nPatients / numlabs));
 
-
-                    try 
-                        if isempty(Values)
-                            theseValues = [];
-                        else
-                            theseValues = Values(jj,:);
-                        end
-                        [simData,simOK,errMessage]  = taskObj.simulate(...
-                                'Names', Names, ...
-                                'Values', theseValues, ...
-                                'OutputTimes', Results.Time);
-                        nSim = nSim + 1;                         
-                        if ~simOK
-
-        %                     ME = MException('simulationRunHelper:simulateVPatients', 'Simulation failed with error: %s', errMessage );
-        %                     throw(ME)
-                            StatusOK = false;
-                            warning('Simulation %d failed with error: %s\n', jj, errMessage);
-                            activeSpec_j = NaN(size(Results.Time,1), length(taskObj.ActiveSpeciesNames));
-                        else
-                            % extract active species data, if specified
-                            if ~isempty(taskObj.ActiveSpeciesNames)
-                                [~,activeSpec_j] = selectbyname(simData,taskObj.ActiveSpeciesNames);
-                            else
-                                [~,activeSpec_j] = selectbyname(simData,taskObj.SpeciesNames);
-                            end
-
-                        end
-
-                    % Add results of the simulation to Results.Data
-                    Results.Data = [Results.Data,activeSpec_j];
-                    catch err% simulation
-                        % If the simulation fails, store NaNs
-                        warning(err.identifier, 'simulationRunHelper: %s', err.message)
-                        % pad Results.Data with appropriate number of NaNs
-                        if ~isempty(taskObj.ActiveSpeciesNames)
-                            Results.Data = [Results.Data,NaN*ones(length(Results.Time),length(taskObj.ActiveSpeciesNames))];
-                        else
-                            Results.Data = [Results.Data,NaN*ones(length(Results.Time),length(taskObj.SpeciesNames))];
-                        end
-
-                        nFailedSims = nFailedSims + 1;
-
-                    end % try
-
-                    send(q, gop(@plus, nSim));
-                    if Cancelled==true
-                        break                        
-                    end
-                end % for jj = ...
+            for labindex = 1:numlabs
+                block = blockSize*(labindex-1) + (1:blockSize);
+                block = block(block<=ItemModel.nPatients);
+                F(labindex) = parfeval(p, @parBlock, 1, block, Names, Values, taskObj, Results);
+            end
+            wait(F);
+            try         
+                Results = fetchOutputs(F);
+            catch err
+            
             end
         end
             
     end % if
     
-    function updateWaitBar(options, ItemModel, jj)
+    function updateWaitBar(WaitBar, ItemModel, jj)
+        nSim = nSim + 1;
         % update wait bar
         StatusOK = true;
-        if ~isempty(options.WaitBar)
-            StatusOK = uix.utility.CustomWaitbar(jj/ItemModel.nPatients, options.WaitBar, sprintf('Simulating vpatient %d/%d', jj, ItemModel.nPatients));
+%         fprintf('jj = %d\n', jj)
+        if ~isempty(WaitBar)
+            StatusOK = uix.utility.CustomWaitbar(jj/ItemModel.nPatients, WaitBar, sprintf('Simulating vpatient %d/%d', nSim, ItemModel.nPatients));
+            if ~StatusOK
+                cancel(F)
+                delete(listener)
+            end
         end
-        if ~StatusOK
-            Cancelled = parallel.pool.Constant(true);
-        end        
+
+    end
+
+    function Results = parBlock(block, Names, Values, taskObj, Results)   
+        
+    
+        nSim = 0;
+%                 disp(block)
+        for jj = block
+%             disp([' ', num2str(jj),' '])
+            % check for user-input parameter values
+            
+            try 
+                if isempty(Values)
+                    theseValues = [];
+                else
+                    theseValues = Values(jj,:);
+                end
+                [simData,simOK,errMessage]  = taskObj.simulate(...
+                        'Names', Names, ...
+                        'Values', theseValues, ...
+                        'OutputTimes', Results.Time);
+                nSim = nSim + 1;                         
+                if ~simOK
+
+%                     ME = MException('simulationRunHelper:simulateVPatients', 'Simulation failed with error: %s', errMessage );
+%                     throw(ME)
+                    StatusOK = false;
+                    warning('Simulation %d failed with error: %s\n', jj, errMessage);
+                    activeSpec_j = NaN(size(Results.Time,1), length(taskObj.ActiveSpeciesNames));
+                else
+                    % extract active species data, if specified
+                    if ~isempty(taskObj.ActiveSpeciesNames)
+                        [~,activeSpec_j] = selectbyname(simData,taskObj.ActiveSpeciesNames);
+                    else
+                        [~,activeSpec_j] = selectbyname(simData,taskObj.SpeciesNames);
+                    end
+
+                end
+
+            % Add results of the simulation to Results.Data
+            Results.Data = [Results.Data,activeSpec_j];
+            catch err% simulation
+                % If the simulation fails, store NaNs
+                warning(err.identifier, 'simulationRunHelper: %s', err.message)
+                % pad Results.Data with appropriate number of NaNs
+                if ~isempty(taskObj.ActiveSpeciesNames)
+                    Results.Data = [Results.Data,NaN*ones(length(Results.Time),length(taskObj.ActiveSpeciesNames))];
+                else
+                    Results.Data = [Results.Data,NaN*ones(length(Results.Time),length(taskObj.SpeciesNames))];
+                end
+
+                nFailedSims = nFailedSims + 1;
+
+            end % try
+
+            send(q, gop(@plus, nSim));                      
+        
+        end % for jj = ...
     end
 
     if usePar
-        Results2 = Results{1};
-        r = {Results{:}};
-        d = cellfun(@(c) c.Data, r, 'UniformOutput', false);
-
-        Results2.Data = horzcat(d{:});
-        Results = Results2;    
-        nFailedSims = sum([nFailedSims{:}]);
+        d = [Results.Data];
+        t = Results(1).Time;
+        s = Results(1).SpeciesNames;
+                
+        r.Data = d;
+        r.Time = t;
+        r.SpeciesNames = s;
+        Results = r;
+               
+        nFailedSims = sum([nFailedSims]);
     end
 %     Message = vertcat(Message{:});
     Cancelled = ~isvalid(options.WaitBar);
