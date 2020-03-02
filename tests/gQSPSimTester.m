0d271475371df217aa83dea000ef38b2a2cdcb9f
Rick Paxson <pax@mathworks.com>
2020-03-02 16:30:34 -0500

    Change testing with CaseStudy2 -> 1 since we have too many warnings with the PCSK9 study.

--- tests/gQSPSimTester.m
+++ tests/gQSPSimTester.m
@@ -2,8 +2,9 @@ classdef gQSPSimTester < QSPViewer.App
     methods
         function obj = gQSPSimTester(filename)
             if nargin == 0
-                rootDirectory = "/Users/pax/work/gQSPsim/Sessions/CaseStudy_aPCSK9/aPCSK9_v7_MES_complete/";
-                filename = rootDirectory + "CaseStudy2_aPCSK9.qsp.mat";
+                %rootDirectory = "/Users/pax/work/gQSPsim/Sessions/CaseStudy_aPCSK9/aPCSK9_v7_MES_complete/";
+                rootDirectory = "/Users/pax/work/gQSPsim/Sessions/CaseStudy_TMDD_complete/";
+                filename = rootDirectory + "CaseStudy1_TMDD.qsp.mat";
             end
             a = obj.loadSessionFromFile(filename, false);            
             obj.Session.RootDirectory = char(rootDirectory);
