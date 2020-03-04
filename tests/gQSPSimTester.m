4b171690dc65338afa19dc98a370d6e18d6a826e
Rick Paxson <pax@mathworks.com>
2020-03-03 22:28:40 -0500

    Try to run tests with 19b since mex is a problem if using 18a on my work machine.

--- tests/gQSPSimTester.m
+++ tests/gQSPSimTester.m
@@ -2,13 +2,18 @@ classdef gQSPSimTester < QSPViewer.App
     methods
         function obj = gQSPSimTester(filename)
             if nargin == 0
-                %rootDirectory = "/Users/pax/work/gQSPsim/Sessions/CaseStudy_aPCSK9/aPCSK9_v7_MES_complete/";
-                rootDirectory = "/Users/pax/work/gQSPsim/Sessions/CaseStudy_TMDD_complete/";
-                filename = rootDirectory + "CaseStudy1_TMDD.qsp.mat";
+                rootDirectory = "/Users/pax/work/gQSPsim/Sessions/CaseStudy_aPCSK9/aPCSK9_v7_MES_complete/";
+                filename = rootDirectory + "CaseStudy2_aPCSK9.qsp.mat";
+                
+                %rootDirectory = "/Users/pax/work/gQSPsim/Sessions/CaseStudy_TMDD/";
+                %filename = rootDirectory + "CaseStudy_TMDD_complete/CaseStudy1_TMDD.qsp.mat";
             end
             a = obj.loadSessionFromFile(filename, false);            
             obj.Session.RootDirectory = char(rootDirectory);
+            %obj.Session.RelativeResultsPath = 'foobar';          
             obj.Session.UseParallel = false;
+            obj.Session.AutoSaveBeforeRun = false;
+            obj.Session.RelativeResultsPath = '../foobar';
         end
         
         function runSimulations(obj)
