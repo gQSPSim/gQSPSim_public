94bc7fd1c1df43ac106c2cddb713f8f8bd5100f0
Justin Feigelman <feigelmj@gene.com>
2020-03-31 21:21:57 -0700

    bug fixes for optimization/default obj function

--- FromGenentech/defaultObj.m
+++ FromGenentech/defaultObj.m
@@ -2,10 +2,10 @@ function objVals = defaultObj(species,data,simTime,dataTime,allData,ID,Grp,currI
 
 allData = allData(~isnan(allData));
 
-if length(allData) > 1
-    objVals = abs(species(:)-data(:))/(max(allData)-min(allData));
+if length(allData) > 1 && range(allData) > 0
+    objVals = abs(species(:)-data(:))/(range(allData));
 else
-    objVals = abs( species(:)-data(:))/abs(data(:));
+    objVals = abs( species(:)-data(:))/mean(data(:));
 end
     
 end
