d09271e8b8bdbe0f9c7e7c90f34cdd2fadc31fd9
Rick Paxson <pax@mathworks.com>
2020-06-09 00:12:30 -0400

    Must add the entire baselines directory to the path. Needed for model supporting files to be on the path.

--- tests/driver.m
+++ tests/driver.m
@@ -1,6 +1,16 @@
 % Script to drive running of gQSPSim tests on gitlab-runner.
-run('../DefinePaths.m');
-results = runtests;
+% Want to put all testing infrastructure in this one directory
+% however we want to run tests from the root gQSPSim directory.
+cd ..
+
+% Add gQSPSim to the path.
+DefinePaths;
+
+% Add test directory to the path. We need various parts of the model
+% available on the path.
+addpath(genpath('tests'));
+
+results = runtests('tests');
 
 if any([results.Failed])
     error('Some errors encountered. See log file for details.');
