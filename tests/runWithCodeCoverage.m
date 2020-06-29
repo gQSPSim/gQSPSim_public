d4758ca9ad22dee70b8029dad41008e8ef310d9c
Rick Paxson <pax@mathworks.com>
2020-06-29 06:41:58 -0400

    Standarize paths for running with code coverage

--- tests/runWithCodeCoverage.m
+++ tests/runWithCodeCoverage.m
@@ -2,9 +2,9 @@ import matlab.unittest.TestSuite
 import matlab.unittest.TestRunner
 import matlab.unittest.plugins.CodeCoveragePlugin
 
-suite = TestSuite.fromFile('tgQSPSim.m');
+suite = TestSuite.fromFile('tests/tgQSPSim.m');
 
 runner = TestRunner.withTextOutput;
 
-runner.addPlugin(CodeCoveragePlugin.forFolder('..', 'IncludingSubfolders', true));
+runner.addPlugin(CodeCoveragePlugin.forFolder('.', 'IncludingSubfolders', true));
 result = runner.run(suite);
