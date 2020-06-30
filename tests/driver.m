93363bb207d230294833b87be79db814da1beed8
Rick Paxson <pax@mathworks.com>
2020-06-30 07:40:32 -0400

    Still trying to get the right exit error codes such that gitlab pipelines fail on either answer failures or any other error condition.

--- tests/driver.m
+++ tests/driver.m
@@ -1,6 +1,7 @@
 % Script to drive running of gQSPSim tests on gitlab-runner.
 % Want to put all testing infrastructure in this one directory
 % however we want to run tests from the root gQSPSim directory.
+currentPWD = pwd;
 cd ..
 
 % Add gQSPSim to the path.
@@ -14,9 +15,16 @@ disp('NOTE: Only running simulation tests.')
 
 try
     results = runtests('tgQSPSim/tSimulations');
-    if any([results.Failed])
-        error('Some errors encountered. See log file for details.');
-    end
 catch e
-    error(e.message)
+    warning(e.message)
+    exit(1)
+end
+
+if any([results.Failed])
+    warning('Some errors encountered. See log file for details.');
+    cd(currentPWD);
+    exit(1)
+else
+    cd(currentPWD);
+    exit
 end
