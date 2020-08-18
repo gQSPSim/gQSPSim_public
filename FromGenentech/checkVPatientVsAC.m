ec9c9edf1a360d89139d83f5088a5a13cb2180f0
Justin Feigelman <feigelman.justin@gene.com>
2020-08-17 22:51:33 -0700

    add error handling for checkVPatientVsAC

--- FromGenentech/checkVPatientVsAC.m
+++ FromGenentech/checkVPatientVsAC.m
@@ -143,6 +143,10 @@ for grpIdx = 1:length(unqGroups) %nItems
             model_outputs = [model_outputs;Inf*ones(length(grpInds),1)];
             LB_outputs = [LB_outputs;NaN(length(grpInds),1)];            
             UB_outputs = [UB_outputs;NaN(length(grpInds),1)];
+            taskName_outputs = [taskName_outputs; NaN(length(grpInds),1)];
+            spec_outputs = [spec_outputs; NaN(length(grpInds),1)];
+            time_outputs = [time_outputs; NaN(length(grpInds),1)];
+                
             StatusOK = false;
             Message = getReport(ME2);
             D = inf;
