ad1c54d79aac78a47c15292f8edcf45a2c04e0f4
Ricardo Paxson <pax@mathworks.com>
2020-08-30 08:12:06 -0400

    Adding tests directory from v2.0

---
+++ tests/tgQSPSim.m
@@ -0,0 +1,41 @@
+classdef tgQSPSim < matlab.unittest.TestCase
+    
+    properties
+        testRootDirectory string
+    end
+    
+    properties (TestParameter)
+        caseStudy = {"baselines/aPCSK9_v7_MES_complete/CaseStudy2_aPCSK9.qsp.mat", ...
+                     "baselines/CaseStudy_TMDD_complete/CaseStudy1_TMDD.qsp.mat"};
+    end
+    
+    methods(TestClassSetup)
+        function foo(testCase)
+            testCase.testRootDirectory = fileparts(mfilename('fullpath'));
+        end
+    end
+    
+    methods(TestMethodSetup)
+    end
+        
+    methods(Test)
+        function tSimulations(testCase, caseStudy)
+            absolutePath = testCase.testRootDirectory + "/" + caseStudy;
+            obj = gQSPSimTester(absolutePath);
+            obj.runSimulations(testCase);
+            delete(obj);
+        end
+        
+        function tOptimizations(testCase, caseStudy)
+            absolutePath = testCase.testRootDirectory + "/" + caseStudy;
+            tester = gQSPSimTester(absolutePath);
+            tester.runOptimizations(testCase);
+            delete(tester);
+        end
+    end
+    
+end
+
+
+
+
