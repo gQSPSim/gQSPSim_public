bf3c6c3d44d5b76681b760eefa0e982810358966
Ricardo Paxson <pax@mathworks.com>
2022-09-27 23:41:52 -0400

    Enable the construction of the controller without a view

--- app/QSPappN.m
+++ app/QSPappN.m
@@ -1,16 +1,19 @@
-function varargout = QSPappN()
+function varargout = QSPappN(useUI)
+    arguments
+        useUI (1,1) logical = true
+    end
 
-    if verLessThan('matlab','9.8') 
-      ThisVer = ver('matlab');
-      warning('QSPAppN is not supported on %s. Use QSPapp instead.', ThisVer.Release);
-    end    
+    if verLessThan('matlab','9.8')
+        ThisVer = ver('matlab');
+        warning('QSPAppN is not supported on %s. Use QSPapp instead.', ThisVer.Release);
+    end
 
     DefinePaths(false);
-    
+
     % run the units script
     registerUnits
-    
-    app = QSPViewerNew.Application.ApplicationUI;
+
+    app = QSPViewerNew.Application.ApplicationUI(useUI);
 
     if nargout == 1
         varargout{1} = app;
