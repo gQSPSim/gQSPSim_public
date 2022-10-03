e66c5d9716139efd4b9782fe20888f7f6b8a5745
Ricardo Paxson <pax@mathworks.com>
2022-10-03 08:42:47 -0400

    Turn on basic UI testing.

--- tests/tsimple.m
+++ tests/tsimple.m
@@ -8,7 +8,7 @@ classdef tsimple < matlab.unittest.TestCase
         function setup(testCase)
             testCase.testRootDirectory = fileparts(mfilename('fullpath'));
             registerUnits;
-%             addpath(genpath('..'));
+            addpath(genpath('..'));
         end
     end
     
@@ -25,49 +25,56 @@ classdef tsimple < matlab.unittest.TestCase
             testCase.assertNotEmpty(session);                        
         end
 
-%         function QSPMenu(testCase)            
-%             a = QSPappN(true);
-%             a.forDebuggingInit;
-% 
-%             cleanup = onCleanup(@()delete(a));
-%             
-%             drawnow;
-% 
-%             testCase.verifyInstanceOf(a, 'QSPViewerNew.Application.Controller', 'ExpectedClass');
-% 
-%             for i = 1:size(a.buildingBlockTypes, 1)
-%                 type = a.buildingBlockTypes{i,2};
-%                 before.(type) = string({a.Sessions.Settings.(type).Name});
-%             end
-% 
-%             for i = 1:size(a.functionalityTypes, 1)
-%                 type = a.functionalityTypes{i,2};
-%                 before.(type) = string({a.Sessions.(type).Name});
-%             end
-% 
-%             % Get the menu items and run the Add Item callback for each
-%             QSPMenu = findobj(a.OuterShell.QSPMenu, 'Text', 'Add New Item');
-%             addItemMenus = QSPMenu.Children;
-%             for i = 1:numel(addItemMenus)
-%                 addItemMenus(i).MenuSelectedFcn();
-%             end
-% 
-%             for i = 1:size(a.buildingBlockTypes, 1)
-%                 type = a.buildingBlockTypes{i,2};
-%                 after.(type) = string({a.Sessions.Settings.(type).Name});
-%             end
-% 
-%             for i = 1:size(a.functionalityTypes, 1)
-%                 type = a.functionalityTypes{i,2};
-%                 after.(type) = string({a.Sessions.(type).Name});
-%             end
-% 
-%             itemsToCheck = fields(after);
-% 
-%             for i = 1:numel(itemsToCheck)
-%                 result = setdiff(after.(itemsToCheck{i}), before.(itemsToCheck{i}));
-%                 testCase.verifyEqual(result, "New " + a.ItemTypes{i,1});
-%             end            
-%         end
+
+
+        function QSPMenu(testCase)
+            % Test all the menus under QSP menu. These add nodes to the
+            % tree.
+            a = QSPappN();
+            
+            a.loadSession('tests/baselines/CaseStudy_TMDD_complete/CaseStudy1_TMDD_pax.qsp.mat');            
+
+            cleanup = onCleanup(@()delete(a));
+            
+            drawnow;
+
+            testCase.verifyInstanceOf(a, 'QSPViewerNew.Application.Controller', 'ExpectedClass');
+
+            for i = 1:size(a.buildingBlockTypes, 1)
+                type = a.buildingBlockTypes{i,2};
+                before.(type) = string({a.Sessions.Settings.(type).Name});
+            end
+
+            for i = 1:size(a.functionalityTypes, 1)
+                type = a.functionalityTypes{i,2};
+                before.(type) = string({a.Sessions.(type).Name});
+            end
+
+            % Get the menu items and run the Add Item callback for each
+            QSPMenu = findobj(a.OuterShell.QSPMenu, 'Text', 'Add New Item');
+            addItemMenus = QSPMenu.Children;
+            for i = 1:numel(addItemMenus)
+                addItemMenus(i).MenuSelectedFcn();
+            end
+
+            for i = 1:size(a.buildingBlockTypes, 1)
+                type = a.buildingBlockTypes{i,2};
+                after.(type) = string({a.Sessions.Settings.(type).Name});
+            end
+
+            for i = 1:size(a.functionalityTypes, 1)
+                type = a.functionalityTypes{i,2};
+                after.(type) = string({a.Sessions.(type).Name});
+            end
+
+            drawnow;
+
+            itemsToCheck = fields(after);
+
+            for i = 1:numel(itemsToCheck)
+                result = setdiff(after.(itemsToCheck{i}), before.(itemsToCheck{i}));
+                testCase.verifyEqual(result, "New " + a.ItemTypes{i,1});
+            end            
+        end
     end
 end
