7cf95f6bf6d6c122bc008e7e955dd2206ac8ef98
Ricardo Paxson <pax@mathworks.com>
2022-10-05 21:52:18 -0400

    New test driver for use at MathWorks. This runs UI tests using gitlab-runner.

---
+++ MW_testDriver.m
@@ -0,0 +1,17 @@
+import matlab.unittest.TestRunner
+import matlab.unittest.Verbosity
+import matlab.unittest.plugins.CodeCoveragePlugin
+import matlab.unittest.plugins.codecoverage.CoberturaFormat
+import matlab.unittest.plugins.XMLPlugin
+
+suite = testsuite('tests', 'Tag', 'RequiresUserInterface');
+
+[~, ~] = mkdir('artifacts');
+
+runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
+runner.addPlugin(CodeCoveragePlugin.forFolder('tests', 'Producing', CoberturaFormat('artifacts/coverage.xml')));
+runner.addPlugin(XMLPlugin.producingJUnitFormat('artifacts/testResults.xml'));
+
+results = runner.run(suite);
+
+assertSuccess(results);
