c493370d2a74dc2cf10af43483cdd454a0bc6580
Ricardo Paxson <pax@mathworks.com>
2022-10-19 12:44:50 -0400

    Cleanup and adding tests

--- tests/tUI.m
+++ tests/tUI.m
@@ -2,68 +2,72 @@ classdef tUI < matlab.uitest.TestCase
     
     properties
         testRootDirectory string
+        ctrl QSPViewerNew.Application.Controller
     end
     
+    properties (TestParameter)        
+        functionalities = {'Simulation', 'Optimization', 'VirtualPopulationGeneration', 'CohortGeneration'};%, 'GlobalSensitivityAnalysis'};
+    end
+
     methods(TestClassSetup)
         function setup(testCase)
             testCase.testRootDirectory = fileparts(mfilename('fullpath'));
+
+            testCase.ctrl = QSPappN();
+            testCase.addTeardown(@delete, testCase.ctrl);            
+
+            % Due to RootDirectory issues we cannot simply use the
+            % controller's api. As a workaround use a direct mat file load
+            % and add the loaded session to the controller.
+            % ctrl.loadSession('tests/baselines/CaseStudy_TMDD_complete/CaseStudy1_TMDD.qsp.mat');            
+            sessionPath = testCase.testRootDirectory + filesep + "baselines" + filesep + "CaseStudy_TMDD_complete" + filesep + "CaseStudy1_TMDD.qsp.mat";
+            SessionContainer = load(sessionPath);
+            rootDirectory = testCase.testRootDirectory + filesep + "baselines" + filesep + "CaseStudy_TMDD_complete";
+            SessionContainer.Session.RootDirectory = rootDirectory;
+            testCase.ctrl.addSession(SessionContainer.Session);
+        end
+    end
+
+    methods(TestClassTeardown)
+        function cleanup(testCase)
+            delete(testCase.ctrl);
         end
     end
     
     methods(Test, TestTags = ["RequiresUserInterface"])
         function pressGitButton(testCase)
-            ctrl = QSPappN();
-            testCase.addTeardown(@delete, ctrl);            
-
-            % Due to RootDirectory issues we cannot simply use the
-            % controller's api. As a workaround use a direct mat file load
-            % and add the loaded session to the controller.
-            % ctrl.loadSession('tests/baselines/CaseStudy_TMDD_complete/CaseStudy1_TMDD.qsp.mat');            
-            sessionPath = testCase.testRootDirectory + filesep + "baselines" + filesep + "CaseStudy_TMDD_complete" + filesep + "CaseStudy1_TMDD.qsp.mat";
-            SessionContainer = load(sessionPath);
-            rootDirectory = testCase.testRootDirectory + filesep + "baselines" + filesep + "CaseStudy_TMDD_complete";
-            SessionContainer.Session.RootDirectory = rootDirectory;
-            ctrl.addSession(SessionContainer.Session);
-            
-            drawnow;
-
             % Pick any functionality and press on it in the tree.
-            testCase.choose(findobj(ctrl.OuterShell.TreeCtrl, 'Tag', 'Simulation').Children(1));
+            testCase.choose(findobj(testCase.ctrl.OuterShell.TreeCtrl, 'Tag', 'Simulation').Children(1));
 
-            gitButton = ctrl.OuterShell.paneToolbar.gitButton;
+            gitButton = testCase.ctrl.OuterShell.paneToolbar.gitButton;
             testCase.press(gitButton);
-            testCase.verifyEqual(ctrl.Sessions.AutoSaveGit, true);
+            testCase.verifyEqual(testCase.ctrl.Sessions.AutoSaveGit, true);
 
-            parallelButton = ctrl.OuterShell.paneToolbar.parallelButton;
+            parallelButton = testCase.ctrl.OuterShell.paneToolbar.parallelButton;
             testCase.press(parallelButton);
-            testCase.verifyEqual(ctrl.Sessions.UseParallel, true);
+            testCase.verifyEqual(testCase.ctrl.Sessions.UseParallel, true);
         end
 
-        function instanceContextMenus(testCase)
-            ctrl = QSPappN();
-            testCase.addTeardown(@delete, ctrl);            
+        function instanceContextMenusFunctionalities(testCase, functionalities)
 
-            % Due to RootDirectory issues we cannot simply use the
-            % controller's api. As a workaround use a direct mat file load
-            % and add the loaded session to the controller.
-            % ctrl.loadSession('tests/baselines/CaseStudy_TMDD_complete/CaseStudy1_TMDD.qsp.mat');            
-            sessionPath = testCase.testRootDirectory + filesep + "baselines" + filesep + "CaseStudy_TMDD_complete" + filesep + "CaseStudy1_TMDD.qsp.mat";
-            SessionContainer = load(sessionPath);
-            rootDirectory = testCase.testRootDirectory + filesep + "baselines" + filesep + "CaseStudy_TMDD_complete";
-            SessionContainer.Session.RootDirectory = rootDirectory;
-            ctrl.addSession(SessionContainer.Session);
+            functionality = findobj(testCase.ctrl.OuterShell.TreeCtrl, 'Tag', functionalities);
 
-            OptimizationFunctionality = findobj(ctrl.OuterShell.TreeCtrl, 'Tag', 'Optimization');
+            preNumberOfFunctionalities = numel(testCase.ctrl.Sessions.(functionalities));
 
             % Must select a node before using its context menu. This might
             % be a bug in the chooseContextMenu functionality.
-            testCase.choose(OptimizationFunctionality.Children(1));
+            testCase.choose(functionality.Children(1));
             
-            testCase.chooseContextMenu(OptimizationFunctionality.Children(1), findobj(OptimizationFunctionality.Children(1).ContextMenu, 'Text', 'Duplicate'));
+            testCase.chooseContextMenu(functionality.Children(1), findobj(functionality.Children(1).ContextMenu, 'Text', 'Duplicate'));
 
-            testCase.verifyNumElements(ctrl.Sessions.Optimization, 2);
-            testCase.verifyEqual({ctrl.Sessions.Optimization.Name}, {'Optimization', 'Optimization_1'});
+            % Expect one more from the duplicate
+            testCase.verifyNumElements(testCase.ctrl.Sessions.(functionalities), preNumberOfFunctionalities + 1);
 
+            % Get all children
+            items = [functionality.Children.NodeData];
+            itemNames = string({items.Name});
+            
+            testCase.verifyTrue(itemNames(1)+"_1" == itemNames(end));            
         end
 
         function deletedItemsContextMenus(testCase)
