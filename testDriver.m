2ef8d37336b4b7b4263f6df036e6b5a36cabcb14
Ricardo Paxson <pax@mathworks.com>
2022-10-19 22:10:10 -0400

    Suppress accepted warnings to avoid large logs. Note the warnings are accepted by the authors of the Test Cases.

--- testDriver.m
+++ testDriver.m
@@ -10,6 +10,10 @@ suite = testsuite('tests', 'Tag', 'NoUI');
 
 [~, ~] = mkdir('artifacts');
 
+% Number of warnings that are allowed by the test cases provided by Genentech.
+% These must be suppressed in order to keep logs at a reasonable size.
+warning('off', 'SimBiology:REACTIONRATE_INVALID');
+
 runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
 runner.addPlugin(CodeCoveragePlugin.forFolder('tests', 'Producing', CoberturaFormat('artifacts/coverage.xml')));
 runner.addPlugin(XMLPlugin.producingJUnitFormat('artifacts/testResults.xml'));
